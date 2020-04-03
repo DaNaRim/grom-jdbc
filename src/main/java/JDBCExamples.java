0c36de5da9bbc3a6609d9f5fd37f6f90959d3511
DaNaRim <rangar.danarim@gmail.com>
2020-04-03 22:40:10 +0300

    lesson 2

---
+++ src/main/java/JDBCExamples.java
@@ -0,0 +1,27 @@
+import java.sql.*;
+
+public class JDBCExamples {
+    private static final String DB_URL = "jdbc:oracle:thin:@gromcode-lessons.c2nwr4ze1uqa.us-east-2.rds.amazonaws.com:1521:ORCL";
+    private static final String USER = "main";
+    private static final String PASS = "PyP2p02rIZ9uyMBpTBwW";
+
+    public static void main(String[] args) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement()) {
+
+//            boolean res = statement.execute("INSERT INTO PRODUCT VALUES (2, 'toy111', 'for childeren', 60)");
+//            System.out.println(res);
+
+//            boolean res = statement.execute("DELETE FROM PRODUCT WHERE  NAME = 'toy111'");
+//            System.out.println(res);
+
+//            int response = statement.executeUpdate("INSERT INTO PRODUCT VALUES (5, 'car', 'for childeren', 70)");
+//            System.out.println(response);
+
+            int response = statement.executeUpdate("DELETE FROM PRODUCT WHERE  NAME = 'car'");
+            System.out.println(response);
+        } catch (SQLException e) {
+            System.err.println("Something went wrong");
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/JDBCFirstStep.java
+++ src/main/java/JDBCFirstStep.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.Date;
 
 public class JDBCFirstStep {
     //1. DB Driver +
@@ -9,13 +10,13 @@ public class JDBCFirstStep {
     //6. close all the connection +
 
     private static final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
-    private static final String DB_URL = "jdbc:oracle:thin:@gromcode-lessons.c88oq4boivpv.eu-west-2.rds.amazonaws.com:1521:ORCL";
+    private static final String DB_URL = "jdbc:oracle:thin:@gromcode-lessons.c2nwr4ze1uqa.us-east-2.rds.amazonaws.com:1521:ORCL";
 
     private static final String USER = "main";
-    private static final String PASS = "AWS_Admin";
+    private static final String PASS = "PyP2p02rIZ9uyMBpTBwW";
 
     public static void main(String[] args) {
-        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement()){
+        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement()) {
             try {
                 Class.forName(JDBC_DRIVER);
             } catch (ClassNotFoundException e) {
@@ -23,13 +24,21 @@ public class JDBCFirstStep {
                 return;
             }
 
-            try (ResultSet resultSet = statement.executeQuery("SELECT * FROM TEST")){
+            try (ResultSet resultSet = statement.executeQuery("SELECT * FROM Orders WHERE PRICE > 5000")) {
                 while (resultSet.next()) {
-                    System.out.println("Object found");
+                    long id = resultSet.getLong(1);
+                    String productName = resultSet.getString(2);
+                    int price = resultSet.getInt(3);
+                    Date dateOrdered = resultSet.getDate(4);
+                    Date dateConfirmed = resultSet.getDate(5);
+
+                    Order order = new Order(id, productName, price, dateOrdered, dateConfirmed);
+
+                    System.out.println(order);
                 }
             }
         } catch (SQLException e) {
-            System.out.println("Something went wrong");
+            System.err.println("Something went wrong");
             e.printStackTrace();
         }
     }
