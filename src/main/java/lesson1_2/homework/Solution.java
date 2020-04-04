21b24ec666362488561da84737c3f4e520dae964
DaNaRim <rangar.danarim@gmail.com>
2020-04-04 14:15:18 +0300

    lesson 2 homework 3

--- src/main/java/lesson1_2/homework/Solution.java
+++ src/main/java/lesson1_2/homework/Solution.java
@@ -1,9 +1,6 @@
 package lesson1_2.homework;
 
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Solution {
     private static final String DB_URL = "jdbc:oracle:thin:@gromcode-lessons.c2nwr4ze1uqa.us-east-2.rds.amazonaws.com:1521:ORCL";
@@ -59,5 +56,39 @@ public class Solution {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public void increasePrice() {
+        try (Statement statement = DriverManager.getConnection(DB_URL, USER, PASS).createStatement()) {
+            statement.executeUpdate("UPDATE PRODUCT SET PRICE = PRICE + 100 WHERE PRICE < 970");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void changeDescription() {
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+             Statement statement = conn.createStatement()) {
+
+            ResultSet resultSet = statement.executeQuery("SELECT ID, DESCRIPTION FROM PRODUCT WHERE LENGTH(DESCRIPTION) > 100");
+
+            while (resultSet.next()) {
+                String[] sentences = resultSet.getString(2).split("\\.");
+
+                sentences[sentences.length - 1] = null;
+
+                String result = "";
+                for (String str : sentences) {
+                    if (str != null) result += str + ".";
+                }
+
+                PreparedStatement prst = conn.prepareStatement("UPDATE PRODUCT SET DESCRIPTION = ? WHERE ID = ?");
+                prst.setString(1, result);
+                prst.setLong(2, resultSet.getLong(1));
+                prst.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
