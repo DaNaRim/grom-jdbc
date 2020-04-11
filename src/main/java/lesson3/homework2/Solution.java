7a91a2411012ac567b3f19b9e39912ccf582ab8f
DaNaRim <rangar.danarim@gmail.com>
2020-04-11 23:52:51 +0300

    lesson 3 homework 2 + sql

---
+++ src/main/java/lesson3/homework2/Solution.java
@@ -0,0 +1,78 @@
+package lesson3.homework2;
+
+import java.sql.*;
+import java.util.Date;
+
+public class Solution {
+    private static final String DB_URL = "jdbc:oracle:thin:@gromcode-lessons.c2nwr4ze1uqa.us-east-2.rds.amazonaws.com:1521:ORCL";
+    private static final String USER = "main";
+    private static final String PASS = "PyP2p02rIZ9uyMBpTBwW";
+
+    //141684
+    public long testSavePerformance() {
+        long start = new Date().getTime();
+        try (PreparedStatement ps = getConnection().prepareStatement("INSERT INTO PRODUCT VALUES (?, 'TEST', 'TEST', 100)")) {
+            for (int i = 0; i < 1000; i++) {
+                ps.setLong(1, i);
+                ps.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return new Date().getTime() - start;
+    }
+
+    //138189
+    public long testDeleteByIdPerformance() {
+        long start = new Date().getTime();
+        try (PreparedStatement ps = getConnection().prepareStatement("DELETE FROM PRODUCT WHERE ID = ?")) {
+            for (int i = 0; i < 1000; i++) {
+                ps.setLong(1, i);
+                ps.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return new Date().getTime() - start;
+    }
+
+    //1437
+    public long testDeletePerformance() {
+        long start = new Date().getTime();
+        try (Statement st = getConnection().createStatement()) {
+            st.executeUpdate("DELETE FROM PRODUCT");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return new Date().getTime() - start;
+    }
+
+    //135622
+    public long testSelectByIdPerformance() {
+        long start = new Date().getTime();
+        try (PreparedStatement ps = getConnection().prepareStatement("SELECT * FROM PRODUCT WHERE ID = ?")) {
+            for (int i = 0; i < 1000; i++) {
+                ps.setLong(1, i);
+                ps.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return new Date().getTime() - start;
+    }
+
+    //1484
+    public long testSelectPerformance() {
+        long start = new Date().getTime();
+        try (Statement st = getConnection().createStatement()) {
+            st.executeUpdate("SELECT * FROM PRODUCT");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return new Date().getTime() - start;
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, USER, PASS);
+    }
+}
