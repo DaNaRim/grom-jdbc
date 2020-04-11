c8795bb208ac19c3c16061d4fd1b25c8c18774ec
DaNaRim <rangar.danarim@gmail.com>
2020-04-11 13:38:24 +0300

    lesson 3

---
+++ src/main/java/lesson3/Demo.java
@@ -0,0 +1,13 @@
+package lesson3;
+
+public class Demo {
+    public static void main(String[] args) {
+        ProductDAO productDAO = new ProductDAO();
+
+        Product product = new Product(10, "test", "test description", 99);
+
+        productDAO.save(product);
+
+        System.out.println(productDAO.getProducts());
+    }
+}
