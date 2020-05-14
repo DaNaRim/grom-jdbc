9f0a3c1959ef002512bcdab2d4274fdc75a5dccb
DaNaRim <rangar.danarim@gmail.com>
2020-05-14 17:08:49 +0300

    lesson 5 homework

--- src/main/java/lesson5/Demo.java
+++ src/main/java/lesson5/Demo.java
@@ -3,7 +3,21 @@ package lesson5;
 import org.hibernate.Session;
 
 public class Demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Product product = new Product();
+        product.setId(98);
+        product.setName("table");
+        product.setDescription("grey & blue");
+        product.setPrice(80);
+
+//        ProductRepository.save(product);
+//        ProductRepository.delete(98);
+//        ProductRepository.update(product);
+
+//        save();
+    }
+
+    public static void save() {
         Session session = new HibernateUtils().createSessionFactory().openSession();
 
         session.getTransaction().begin();
