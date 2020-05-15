64e75aa4a122a7031cfb6d889e0bf3a62901d92c
DaNaRim <rangar.danarim@gmail.com>
2020-05-15 13:07:27 +0300

    Hibernate: lesson 1 fixes

--- src/main/java/hibernate/lesson1/ProductRepository.java
+++ src/main/java/hibernate/lesson1/ProductRepository.java
@@ -5,25 +5,20 @@ import org.hibernate.Session;
 public class ProductRepository {
 
     public static void save(Product product) throws Exception {
-        Session session = new HibernateUtils().createSessionFactory().openSession();
-        try {
+        try (Session session = new HibernateUtils().createSessionFactory().openSession()) {
             session.getTransaction().begin();
 
             session.save(product);
 
             session.getTransaction().commit();
         } catch (Exception e) {
-            session.getTransaction().rollback();
             throw new Exception("An error occurred while trying to save product " + product.getId() + " : " +
                     e.getMessage());
-        } finally {
-            session.close();
         }
     }
 
     public static void delete(long id) throws Exception {
-        Session session = new HibernateUtils().createSessionFactory().openSession();
-        try {
+        try (Session session = new HibernateUtils().createSessionFactory().openSession()) {
             session.getTransaction().begin();
 
             Product product = new Product();
@@ -32,28 +27,21 @@ public class ProductRepository {
 
             session.getTransaction().commit();
         } catch (Exception e) {
-            session.getTransaction().rollback();
             throw new Exception("An error occurred while trying to save product " + id + " : " +
                     e.getMessage());
-        } finally {
-            session.close();
         }
     }
 
     public static void update(Product product) throws Exception {
-        Session session = new HibernateUtils().createSessionFactory().openSession();
-        try {
+        try (Session session = new HibernateUtils().createSessionFactory().openSession()) {
             session.getTransaction().begin();
 
             session.update(product);
 
             session.getTransaction().commit();
         } catch (Exception e) {
-            session.getTransaction().rollback();
             throw new Exception("An error occurred while trying to update product " + product.getId() + " : " +
                     e.getMessage());
-        } finally {
-            session.close();
         }
     }
 }
